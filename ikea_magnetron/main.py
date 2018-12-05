9b9c3ec835d770869a30f337d2c364112dc88b8f
Erna Oudman <ernaoudman@hotmail.com>
2018-12-05 21:11:22 +0100

    met webserver, maar nu nog async

--- ikea_magnetron/main.py
+++ ikea_magnetron/main.py
@@ -1,6 +1,8 @@
 import tm1637
 from machine import Pin
 import time
+import network
+import socket
 tm = tm1637.TM1637(clk=Pin(5), dio=Pin(4))
 
 # Rotary encoder
@@ -12,11 +14,26 @@ r = RotaryIRQ(
     max_val=200,
 )
 
+wifi = network.WLAN(network.AP_IF)
+wifi.active(True)
+print(wifi.ifconfig())
+ipadr = wifi.ifconfig()[0]
+
+html = "<!DOCTYPE html><html><h1>Ik doe het!</h1></html>"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((ipadr, 80))
+s.listen(5)
+
 while True:
+    conn, addr = s.accept()
+    request=conn.recv(1024)
     cur_val = r.value()
     # print(r.value())
     minutes = int(cur_val/60)
     seconds = cur_val%60
-    tm.numbers(minutes,seconds)
-    time.sleep(.1)
-   
+    # tm.numbers(minutes,seconds)
+    print('minutes: {}, seconds: {}'.format(minutes,seconds))
+    time.sleep(.2)
+    conn.send(html)
+    conn.close()
